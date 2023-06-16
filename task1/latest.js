e5d2d40e86cccf0180bfbcb576a9be09eafbf240
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-06-16 11:12:34 +0100

    added leaflet map

--- task1/latest.js
+++ task1/latest.js
@@ -12,10 +12,9 @@ let address2 = document.getElementById("address2");
 let submit = document.getElementById("submitButton");
 
 
-
 function main() {
-    let pointAValue = pointA.value
-    let pointBValue = pointB.value
+    let pointAValue = pointA.value.trim()
+    let pointBValue = pointB.value.trim()
 
     var split_lat_lng_A = pointAValue.split(',');
     var split_lat_lng_B = pointBValue.split(',');
@@ -65,37 +64,108 @@ main()
 
 });
 
+
+
+
 submit.addEventListener("submit", (e) => {
   e.preventDefault();
 
   console.log(address1.value);
+  console.log(address2.value);
 
-  const apiUrl = `https://geocode.search.hereapi.com/v1/geocode?q=${address1.value}&apiKey=nVPx6P5jZSJ77ZBrOFtlic408AOmJbLtjr3PxPoTil0`;
+  const apiUrl1 = `https://geocode.search.hereapi.com/v1/geocode?q=${encodeURIComponent(
+    address1.value
+  )}&apiKey=nVPx6P5jZSJ77ZBrOFtlic408AOmJbLtjr3PxPoTil0`;
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
+  fetch(apiUrl1)
+    .then((response) => response.json())
+    .then((data) => {
       // Process the fetched data here
       console.log(data);
-  
-      let latA =  data['items'][0]['position']['lat'];
-      let lngA =  data['items'][0]['position']['lng'];
-  
+
+      let latA = data.items[0].position.lat;
+      let lngA = data.items[0].position.lng;
+
       console.log(latA);
-      console.log(lngA);     
-      
-  })
-  
-    .catch(error => {
+      console.log(lngA);
+
+      const apiUrl2 = `https://geocode.search.hereapi.com/v1/geocode?q=${encodeURIComponent(
+        address2.value
+      )}&apiKey=nVPx6P5jZSJ77ZBrOFtlic408AOmJbLtjr3PxPoTil0`;
+
+      fetch(apiUrl2)
+        .then((response) => response.json())
+        .then((data) => {
+          // Process the fetched data here
+          console.log(data);
+
+          let latB = data.items[0].position.lat;
+          let lngB = data.items[0].position.lng;
+
+          console.log(latB);
+          console.log(lngB);
+
+          const long_difference = lngB - lngA;
+          const lat_difference = latB - latA;
+
+          console.log(long_difference);
+          console.log(lat_difference);
+
+          // Perform further calculations or operations with the obtained latitude and longitude values
+
+          // Using Haversine formula
+          const a =
+            Math.sin((lat_difference * Math.PI) / 180 / 2) *
+              Math.sin((lat_difference * Math.PI) / 180 / 2) +
+            Math.cos((latA * Math.PI) / 180) *
+              Math.cos((latB * Math.PI) / 180) *
+              Math.sin((long_difference * Math.PI) / 180 / 2) *
+              Math.sin((long_difference * Math.PI) / 180 / 2);
+
+          const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+          // Radius of the Earth in kilometers
+          const earthRadius = 6371;
+
+          // Final distance
+          const distance = earthRadius * c;
+
+          // To 2 decimal places
+          const sf_distance = distance.toFixed(2);
+
+          console.log(sf_distance);
+          const mapElement = document.querySelector("#map");
+          mapElement.style.height= "300px";
+
+          var map = L.map("map").setView([latA, lngA], 8);
+
+          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }).addTo(map);
+
+          L.marker([latA, lngA])
+            .addTo(map)
+            .bindPopup(`Distance is ${sf_distance} km`)
+            .openPopup();
+
+          L.marker([latB, lngB])
+            .addTo(map)
+            .bindPopup(`Distance is ${sf_distance} km`)
+            .openPopup();
+        })
+        .catch((error) => {
+          // Handle any errors that occurred during the fetch
+          console.error("Error:", error);
+        });
+    })
+    .catch((error) => {
       // Handle any errors that occurred during the fetch
-      console.error('Error:', error);
-    });
-  
+      console.error("error", error);
 
+    })
 
-});
-
-
+  })
 
 
 
